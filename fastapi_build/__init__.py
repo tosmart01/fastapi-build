4813520839939ab2c0e932b506df5df0024690bf
wuwukai <wangzhuo@datagrand.com>
2024-06-07 15:17:12 +0800

    update version

--- fastapi_build/__init__.py
+++ fastapi_build/__init__.py
@@ -3,7 +3,7 @@
 # @Author : PinBar
 # @File : __init__.py.py
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 
 
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ clean_pyc_and_cache()
 
 setup(
     name="fastapi_build",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     url="https://github.com/tosmart01/fastapi-build",
     include_package_data=True,
